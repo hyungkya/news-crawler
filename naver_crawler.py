8a7098424666338678cc2f5dba6c4d26749219f0
hyungkya <36350256+hyungkya@users.noreply.github.com>
2024-01-31 15:00:04 +0900

    refactor: naver_crawler.py만 실행해도 되게끔 구조 변경

--- naver_crawler.py
+++ naver_crawler.py
@@ -1,5 +1,8 @@
 import requests
 import os
+import xml.etree.ElementTree as ET
+import visitor
+import pandas as pd
 
 header = {
     "Content-Type": "application/json; charset=UTF-8",
@@ -24,3 +27,13 @@ response = requests.get(url, headers=header)
 
 with open("links.xml", "w") as f:
     f.write(response.text)
+
+sentences = []
+tree = ET.ElementTree(file="links.xml")
+
+for item in tree.findall("./channel/item"):
+    sentences.extend(visitor.request_and_parse(item.find("link").text))
+
+df = pd.DataFrame(sentences, columns=["sentence"])
+df.insert(1, "sentiment", -1, True)
+df.to_csv("sentences.csv", index=False)
